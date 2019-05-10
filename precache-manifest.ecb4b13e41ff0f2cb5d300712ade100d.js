08e8b2b298e5138fc42b31d5674871e073022857
zshawk1982 <zshawk1982@163.com>
2019-05-10 22:31:52 +0800

    'first'

---
+++ precache-manifest.ecb4b13e41ff0f2cb5d300712ade100d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "d215473030ca04a8db3c044c3e05a2d5",
+    "url": "/static/media/other_selected.d2154730.svg"
+  },
+  {
+    "revision": "b72e95e670dd87a980ad",
+    "url": "/static/css/main.de443531.chunk.css"
+  },
+  {
+    "revision": "b6e7b586b40c7afe4083444659a5ff5c",
+    "url": "/static/media/banner2.b6e7b586.jpg"
+  },
+  {
+    "revision": "fa0e10a11fa5cc948255",
+    "url": "/static/js/1.fa0e10a1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "34ba102861fc8c98b9a5663046b89464",
+    "url": "/static/media/home_selected.34ba1028.svg"
+  },
+  {
+    "revision": "5298f7e70cecb7f7825808bf569166a1",
+    "url": "/static/media/info_selected.5298f7e7.svg"
+  },
+  {
+    "revision": "f83de09cb6ea400ca2348e81b1f46af9",
+    "url": "/static/media/message_selected.f83de09c.svg"
+  },
+  {
+    "revision": "b72e95e670dd87a980ad",
+    "url": "/static/js/main.b72e95e6.chunk.js"
+  },
+  {
+    "revision": "c8facd8e67dafc02f8dc7e3f032e2baf",
+    "url": "/static/media/home.c8facd8e.svg"
+  },
+  {
+    "revision": "d5d339b1ca172d5af9c545cc0cf43fc8",
+    "url": "/static/media/info.d5d339b1.svg"
+  },
+  {
+    "revision": "6e0e6811d4804cd5fc7ec723225e0cb2",
+    "url": "/static/media/message.6e0e6811.svg"
+  },
+  {
+    "revision": "1abe4abd786360f7ec39771cddf3506a",
+    "url": "/static/media/other.1abe4abd.svg"
+  },
+  {
+    "revision": "b8dd1e51bd442f20932d0bae8243052f",
+    "url": "/static/media/banner0.b8dd1e51.jpg"
+  },
+  {
+    "revision": "b72221abc6e81395b3c2030ae6314bb5",
+    "url": "/static/media/banner1.b72221ab.jpg"
+  },
+  {
+    "revision": "fa0e10a11fa5cc948255",
+    "url": "/static/css/1.de4c50bb.chunk.css"
+  },
+  {
+    "revision": "6d5d6b903b51c87be2a6b15c97a8ff2f",
+    "url": "/index.html"
+  }
+];
